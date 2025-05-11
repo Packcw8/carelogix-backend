92e39d8cb01ce4d696cc2e690ca09c2187d186f8
Cody Pack <packcw8@gmail.com>
2025-05-11 00:35:09 -0400

    Fix import path for create_admins.py on Render

--- app/scripts/create_admins.py
+++ app/scripts/create_admins.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 # scripts/create_admins.py
 import uuid
 from app.models import User, Agency
